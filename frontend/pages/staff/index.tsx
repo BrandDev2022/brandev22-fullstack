c7f057b008cf065f262505141d6e489c83b91734
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-10-30 07:26:26 -0400

    set image type

--- frontend/pages/staff/index.tsx
+++ frontend/pages/staff/index.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import styles from "../../styles/Staff.module.css";
 import AlbertPic from "../../public/media/AlbertPic.png";
 import React, { useContext } from "react";
@@ -9,7 +9,7 @@ export interface Employee {
   lastName: string;
   summary: string;
   title: string;
-  picture: string;
+  picture: StaticImageData;
 }
 const AlbertPhoto =
   "https://images.unsplash.com/photo-1552982729-0ae6a5658d70?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cHJvZmVzc2lvbiUyMHBob3RvfGVufDB8fDB8fHww";
@@ -24,11 +24,18 @@ const StaffPage = () => {
       </div>
       {StaffPage?.Employees.map((each, idx) => (
         <div className={styles.staffCard} key={idx}>
-          <Image src={each.picture} alt="person" width={250} height={300} />
+          <Image
+            style={{ borderRadius: "500px" }}
+            src={each.picture}
+            alt="person"
+            width={250}
+            height={300}
+          />
           <div className={styles.staffContent}>
             <h2 style={{ textDecoration: "underline" }}>
-              {each.firstName} {each.lastName}: {each.title}
+              {each.firstName} {each.lastName}
             </h2>
+            <h3>{each.title}</h3>
             <p>{each.summary}</p>
           </div>
         </div>
